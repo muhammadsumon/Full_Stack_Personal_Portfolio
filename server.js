f89fc4aedbc9f0c1324e3ad796be233c7bb4480e
muhammadsumon <muhammadsumon.me@gmail.com>
2022-08-19 04:30:27 +0600

    Created server for serving react app

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 9000, () => {
+  console.log("Server is running successfully")
+});
