e3fdab41b247ca0fd356b3980772f5a1556bae79
muhammadsumon <muhammadsumon.me@gmail.com>
2022-08-20 02:47:03 +0600

    Changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-const REACT_APP_Api_Url = process.env.REACT_APP_Api_Url;
-const REACT_APP_Api_Key = process.env.REACT_APP_Api_Key;
+const REACT_APP_Api_Url = "https://api-muhammadsumon.herokuapp.com";
+const REACT_APP_Api_Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3RoaW5nIjoibm90aGluZyIsImlhdCI6MTY2MDkzOTM3MH0.V4WhbBPWqbNdjRzFHzqjyeBA4ccRw3Lej19Bl9KpGjc";
 
 export {
     REACT_APP_Api_Url, REACT_APP_Api_Key
