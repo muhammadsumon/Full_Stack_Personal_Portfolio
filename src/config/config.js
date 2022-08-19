e629602c3bb1ad47de077a0c9821fb998bc36160
muhammadsumon <muhammadsumon.me@gmail.com>
2022-08-20 02:54:20 +0600

    Changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const REACT_APP_Api_Url = "https://api-muhammadsumon.herokuapp.com";
-const REACT_APP_Api_Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3RoaW5nIjoibm90aGluZyIsImlhdCI6MTY2MDkzOTM3MH0.V4WhbBPWqbNdjRzFHzqjyeBA4ccRw3Lej19Bl9KpGjc";
+const REACT_APP_Api_Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3RoaW5nIjoibm90aGluZyIsImlhdCI6MTY2MDk0MjQ0N30.uXXb5pzQeXNX_uh8t4jbmISqkIU0nhfYo9YRt-teS84";
 
 export {
     REACT_APP_Api_Url, REACT_APP_Api_Key
