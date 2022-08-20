754b0e0665e6a59925811c1019ac1eed3cd039f6
muhammadsumon <muhammadsumon.me@gmail.com>
2022-08-20 08:19:16 +0600

    Test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', (req, res) => {
