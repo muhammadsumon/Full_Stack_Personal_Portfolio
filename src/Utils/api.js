07ca8911d907c6b1d8646016a99be355dfc1b943
muhammadsumon <muhammadsumon.me@gmail.com>
2022-08-21 01:13:06 +0600

    changed api url

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -4,7 +4,7 @@ import { React_App_Public_Api_Key } from "../config/config";
 const fetcher = (url) => {
     return axios.get(url, {
         headers: {
-            Authorization: `Bearer ${React_App_Public_Api_Key}`,
+            Authorization: `Bearer ${React_App_Public_Api_Key}`
         },
     });
 }
--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const React_App_Api_Url = "https://api-muhammadsumon.herokuapp.com";
-export const React_App_Public_Api_Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3RoaW5nIjoibm90aGluZyIsImlhdCI6MTY2MDk1NTIzNn0.bPG4L - XIixmtIT - 0h7pHZAzKM47H6VTQu4jxVSKlrj8";
+export const React_App_Api_Url = "https://api.muhammadsumon.me";
+export const React_App_Public_Api_Key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub3RoaW5nIjoibm90aGluZyIsImlhdCI6MTY2MTAyMjAyNH0.rWRBVhsizMc0W4cC4Vw2KPVcPkNoXzgMuLFnx0tL5Sk";
